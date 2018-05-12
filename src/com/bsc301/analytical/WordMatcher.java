3fc872328b6646deaf4a312c28857d7e1636f2f4
Mat <mathewcausby@hotmail.com>
2018-05-12 08:38:29 +0800

    Levenshtein Distance Calculation

--- src/com/bsc301/analytical/WordMatcher.java
+++ src/com/bsc301/analytical/WordMatcher.java
@@ -41,15 +41,39 @@ public class WordMatcher
                 {
                     if(word.toLowerCase().contains(phobiaWord))
                     {
-                        MatchedPhobia match = new MatchedPhobia(phobia, sentence);
-                        matchedPhobias.add(match);
-                        System.out.println(sentence + " -> " + phobia.GetPhobia());
+                        if(LevenshteinDistance(word.toLowerCase(), phobiaWord) <= 1)
+                        {
+                            MatchedPhobia match = new MatchedPhobia(phobia, sentence);
+                            matchedPhobias.add(match);
+                            System.out.println(sentence + " -> " + phobia.GetPhobia());
+                        }
                     }
                 }
             }
         }
         
         return matchedPhobias;
+    }
+    
+    private int LevenshteinDistance(String wordA, String wordB)
+    {
+        int[] costs = new int[wordB.length() + 1];
+        for(int i =0; i < costs.length; i++)
+        {
+            costs[i] = i;
+        }
+        for(int i = 1; i <= wordA.length(); i++)
+        {
+            costs[0] = i;
+            int k = i - 1;
+            for(int j = 1; j <= wordB.length(); j++)
+            {
+                int l = Math.min(1 + Math.min(costs[j], costs[j - 1]), wordA.charAt(i - 1) == wordB.charAt(j - 1) ? k : k + 1);
+                k = costs[j];
+                costs[j] = l;
+            }
+        }
+        return costs[wordB.length()];
     }
     
     
